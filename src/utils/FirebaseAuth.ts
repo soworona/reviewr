025427333b3ee491382f4e5ebf8ba851f6656825
Sworna <imsworna@gmail.com>
2025-08-05 13:27:07 +0545

    added handleLoginWithEmail

--- src/utils/FirebaseAuth.ts
+++ src/utils/FirebaseAuth.ts
@@ -6,22 +6,43 @@ export async function handleSignUpWithEmail(
   email: string,
   password: string,
 ) {
-  try{
+  try {
     await firebase.auth().createUserWithEmailAndPassword(email, password);
     console.log('Signup success');
     return true;
-  }catch(error: any) {
-      let message = 'Something went wrong';
+  } catch (error: any) {
+    let message = 'Something went wrong';
 
-      if (error.code === 'auth/email-already-in-use') {
-        message = 'That email is already in use!';
-      } else if (error.code === 'auth/invalid-email') {
-        message = 'That email is invalid!';
-      } else if (error.code === 'auth/weak-password') {
-        message = 'Password should be at least 6 characters.';
-      }
+    if (error.code === 'auth/email-already-in-use') {
+      message = 'That email is already in use!';
+    } else if (error.code === 'auth/invalid-email') {
+      message = 'That email is invalid!';
+    } else if (error.code === 'auth/weak-password') {
+      message = 'Password should be at least 6 characters.';
+    }
 
-      console.log('error', message);
-      return false;
-    };
+    console.log('error', message);
+    return false;
+  }
+}
+
+export async function handleLogin(email: string, password: string) {
+  try {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+    console.log('sign in success');
+
+    return true;
+  } catch (error: any) {
+    let message = 'Something went wrong';
+
+    if (error.code === 'auth/invalid-email') {
+      message = 'That email is invalid!';
+    } else if (error.code === 'auth/user-not-found') {
+      message = 'No account found with this email';
+    } else if (error.code === 'auth/wrong-password') {
+      message = 'Incorrect password';
+    }
+    console.log('error', message);
+    return false;
+  }
 }
