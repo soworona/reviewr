3a1570327e6524fe6f4842063a6bd3c3fcd55a1d
Sworna <imsworna@gmail.com>
2025-08-05 13:12:04 +0545

    added HomeScreen

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from './type';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
+import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -10,6 +11,7 @@ export default function AuthStack() {
     <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen}  />
       <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
 }
