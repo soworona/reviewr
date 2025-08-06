04dbb01a8c135f87a65ad69d9509f6b7cacc0e98
Sworna <imsworna@gmail.com>
2025-08-06 14:26:45 +0545

    fetch data from api and show to card

--- src/componets/CardComponent.tsx
+++ src/componets/CardComponent.tsx
@@ -1,32 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from 'react-native';
 
-const CardComponent = () => {
-    return(
-        <View style={styles.container}>
-            <View style={styles.img} />
-            <Text style={styles.heading} numberOfLines={2} ellipsizeMode="tail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas modi consequatur eos, architecto veniam obcaecati consequuntur odit, dolorem cumque, nihil tenetur corporis natus laborum. Animi saepe recusandae iste rerum facere!</Text>
-        </View>
-    )
-}
+const CardComponent = ({ movie }: { movie: any }) => {
+  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: imageUrl }} style={styles.img} resizeMode="cover" />
+      <Text style={styles.heading} numberOfLines={2} ellipsizeMode="tail">
+        {movie.title}
+      </Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        gap: 8,
-        maxWidth:120,
-        maxHeight:213
-    }, 
-    img:{
-        width:120,
-        height:160, 
-        borderRadius:8,
-        backgroundColor:'pink'
-    },
-    heading:{
-        color:'white',
-        fontWeight:300,
-        fontSize:14,
-    }
-
-})
+  container: {
+    gap: 8,
+    maxWidth: 120,
+    maxHeight: 213,
+  },
+  img: {
+    width: 120,
+    height: 160,
+    borderRadius: 8,
+    backgroundColor: 'pink',
+  },
+  heading: {
+    color: 'white',
+    fontWeight: 300,
+    fontSize: 14,
+  },
+});
 
 export default CardComponent;
--- src/componets/CarouselComponent.tsx
+++ src/componets/CarouselComponent.tsx
@@ -1,20 +1,53 @@
 import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 import CardComponent from './CardComponent';
+import Config from 'react-native-config';
+import { useEffect, useState } from 'react';
+import Toast from 'react-native-toast-message';
 
 type CarouselComponentProps = {
   label: string;
-}
+};
 const CarouselComponent = (props: CarouselComponentProps) => {
-    const dummyData = ['1', '2', '3', '4', '5'];
+  const [movies, setMovies] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getNowPlaying = async () => {
+      try {
+        const axios = require('axios').default;
+        const response = await axios.get(
+          'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',
+          {
+            headers: {
+              accept: 'application/json',
+              Authorization: `Bearer ${Config.API_BEARER_TOKEN}`,
+            },
+          },
+        );
+        setMovies(response.data.results);
+        console.log('response', response.data.results);
+      } catch (err) {
+        Toast.show({
+          type: 'error',
+          text1: 'Try again!',
+          text2: 'Something went wrong',
+        });
+      }
+    };
+
+    getNowPlaying();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>{props.label}</Text>
-          <FlatList
-        data={dummyData}
+      <FlatList
+        data={movies}
         horizontal
         showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => item}
-        renderItem={() => <CardComponent />}
+        keyExtractor={(item, index) =>
+          item?.id ? item.id.toString() : index.toString()
+        }
+        renderItem={({ item }) => <CardComponent movie={item} />}
         contentContainerStyle={styles.carousel}
       />
     </View>
