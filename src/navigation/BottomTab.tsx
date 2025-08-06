89bf42fe7402675d8c995663f53d66b120aee3cb
Sworna <imsworna@gmail.com>
2025-08-07 00:17:10 +0545

    ui update

--- src/navigation/BottomTab.tsx
+++ src/navigation/BottomTab.tsx
@@ -4,42 +4,103 @@ import HomeScreen from '../screens/bottom-tabs/HomeScreen';
 import ProfileScreen from '../screens/bottom-tabs/ProfileScreen';
 import SearchScreen from '../screens/bottom-tabs/SearchScreen';
 import WishlistScreen from '../screens/bottom-tabs/WishlistScreen';
-import Octicons  from '@react-native-vector-icons/octicons';
-import MaterialDesignIcons from '@react-native-vector-icons/material-design-icons';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const Tab = createBottomTabNavigator<BottomTabParamList>();
 
 const BottomTab = () => {
   return (
-    <Tab.Navigator screenOptions={({route}) =>({
-              tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconPath;
 
-          if (route.name === 'Home') {
-            iconName = 'home'
-          }else if (route.name === 'Search'){
-            iconName = 'search'
-          }else if ( route.name === 'Profile'){
-            iconName = 'profile'
-          }else {
-            iconName = 'book-outline'
+          switch (route.name) {
+            case 'Home':
+              iconPath = focused
+                ? require('../../src/assets/icons/Home-yellow.png')
+                : require('../../src/assets/icons/Home.png');
+              break;
+            case 'Search':
+              iconPath = focused
+                ? require('../../src/assets/icons/Search-yellow.png')
+                : require('../../src/assets/icons/Search.png');
+              break;
+            case 'Wishlist':
+              iconPath = focused
+                ? require('../../src/assets/icons/Bookmark-yellow.png')
+                : require('../../src/assets/icons/Bookmark.png');
+              break;
+            case 'Profile':
+              iconPath = focused
+                ? require('../../src/assets/icons/Profile-yellow.png')
+                : require('../../src/assets/icons/Profile.png');
+              break;
           }
 
-          return <MaterialDesignIcons name={iconName} size={size} color={color} />;
+          return <Image source={iconPath} style={styles.icon} />;
         },
-        tabBarStyle:{
-            backgroundColor:'#001C29',
-            height:70
+
+        tabBarStyle: {
+          backgroundColor: '#001C29',
+          borderTopWidth: 0,
         },
-        tabBarInactiveTintColor:'white',
-        tabBarActiveTintColor:'#FFB703'
-    })}>
+
+        headerStyle: {
+          backgroundColor: '#002335',
+          elevation: 0,
+          borderBottomWidth: 0,
+        },
+        headerTitle: '',
+        headerLeft: () => {
+          if (route.name === 'Home') {
+            return (
+              <Image
+                source={require('../../src/assets/LOGO.png')}
+                style={{ width: 185, height: 45, resizeMode: 'contain' }}
+              />
+            );
+          }
+          return (
+            <Text style={{ color: 'white', fontSize: 18, marginLeft:20 }}>{route.name}</Text>
+          );
+        },
+        headerRight: () => {
+          if (route.name === 'Profile') {
+            return <Text style={styles.logoutBtn}>Logout</Text>;
+          }
+        },
+        tabBarLabelStyle: {
+          marginTop: 5,
+        },
+        tabBarInactiveTintColor: 'white',
+        tabBarActiveTintColor: '#FFB703',
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Wishlist" component={WishlistScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
+    resizeMode: 'contain',
+  },
+  logoutBtn: {
+    marginRight:20,
+    color:'white',
+    backgroundColor:'#FFB703',
+    paddingVertical:10,
+    paddingHorizontal:15,
+    borderRadius:10,
+    fontWeight:600,
+    fontSize:15
+  }
+});
 
 export default BottomTab;
