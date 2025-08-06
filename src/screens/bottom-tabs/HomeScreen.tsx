4a5e3ccea5dbc7a8fcb95863c2b19ca0437b8617
Sworna <imsworna@gmail.com>
2025-08-06 12:27:15 +0545

    made view scrollable

--- src/screens/bottom-tabs/HomeScreen.tsx
+++ src/screens/bottom-tabs/HomeScreen.tsx
@@ -1,35 +1,43 @@
-import { StyleSheet, Text, View } from "react-native"
-import CarasoulComponent from "../../componets/CarouselComponent";
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import CarasoulComponent from '../../componets/CarouselComponent';
 
 const HomeScreen = () => {
-    return(
-        <View style={styles.container}>
-            <Text style={styles.heading}>Welcome back, <Text style = {{color: '#FFCA45'}}>Dilhara</Text>!</Text>
-            <Text style={styles.subheading}>Review or log film you’ve watched...</Text>
+  return (
+    <ScrollView style={styles.container}>
+      <View>
 
-            <CarasoulComponent />
-        </View>
-    )
-}
+      <Text style={styles.heading}>
+        Welcome back, <Text style={{ color: '#FFCA45' }}>Dilhara</Text> !
+      </Text>
+      <Text style={styles.subheading}>
+        Review or log film you’ve watched...
+      </Text>
+      </View>
+
+      <CarasoulComponent />
+      <CarasoulComponent />
+      <CarasoulComponent />
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-    flex: 1, 
-    backgroundColor:'#002335',
-    paddingVertical:21,
-    paddingHorizontal: 13
-    },
-    heading: {
-        color:'white',
-        fontSize:20,
-        fontWeight: 700,
-        marginTop:25
-    },
-    subheading:{
-        color:'white',
-        fontSize: 14,
-        fontWeight: 400,
-        marginBottom:25
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#002335',
+    paddingVertical: 21,
+    paddingHorizontal: 13,
+    gap:25
+  },
+  heading: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 700,
+  },
+  subheading: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: 400
+  },
+});
 export default HomeScreen;
