e01fbfe656f90abeeae430f63cac4a2885090087
Sworna <imsworna@gmail.com>
2025-08-08 12:13:07 +0545

    created search bar component

--- src/screens/bottom-tabs/SearchScreen.tsx
+++ src/screens/bottom-tabs/SearchScreen.tsx
@@ -1,11 +1,21 @@
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
+import SearchBarComponent from "../../componets/SearchBarComponent";
 
 const SearchScreen = () => {
     return(
-        <View>
-            <Text>Search</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <SearchBarComponent />
+        </SafeAreaView>
     )
 }
 
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#052433ff',
+        flex:1,
+        paddingVertical:30,
+        alignItems:'center'
+    }
+})
 export default SearchScreen;
