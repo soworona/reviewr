02848ed5cc2dbfaf48179e12a50731d13b3f38e5
Sworna <imsworna@gmail.com>
2025-08-09 17:06:17 +0545

    created wishlist ui

--- src/screens/bottom-tabs/WishlistScreen.tsx
+++ src/screens/bottom-tabs/WishlistScreen.tsx
@@ -3,7 +3,12 @@ import { StyleSheet, Text, View } from "react-native"
 const WishlistScreen = () => {
     return(
         <View style={styles.container}>
-            <Text>Wishlist</Text>
+            <View style={styles.img} />
+            <View style={styles.img} />
+            <View style={styles.img} />
+            <View style={styles.img} />
+             <View style={styles.img} />
+            <View style={styles.img} />
         </View>
     )
 }
@@ -11,7 +16,21 @@ const WishlistScreen = () => {
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor:'#052433ff'
+        backgroundColor:'#002335',
+        paddingHorizontal:21,
+        gap:6,
+        flexDirection:'row',
+        alignContent: 'flex-start',
+        flexWrap:'wrap',
+        paddingTop:30
+    },
+    img:{
+        maxWidth:80,
+        minWidth:80,
+        flex:1,
+        height:110,
+        backgroundColor:'pink',
+        borderRadius:8
     }
 })
 
