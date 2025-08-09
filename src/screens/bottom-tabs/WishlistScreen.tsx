6b3e97fcb1cd119d5402f1b648024e48151aac71
Sworna <imsworna@gmail.com>
2025-08-09 12:36:28 +0545

    wishlist ui design(incomplete)

--- src/screens/bottom-tabs/WishlistScreen.tsx
+++ src/screens/bottom-tabs/WishlistScreen.tsx
@@ -1,11 +1,18 @@
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 
 const WishlistScreen = () => {
     return(
-        <View>
+        <View style={styles.container}>
             <Text>Wishlist</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#052433ff'
+    }
+})
 
 export default WishlistScreen;
