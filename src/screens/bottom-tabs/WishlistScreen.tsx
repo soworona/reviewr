aad2a4208f4062e1d302bc5a2fc5ab1d968b2417
Sworna <imsworna@gmail.com>
2025-08-11 14:31:13 +0545

    made wishlist update automatically when screen gains focus

--- src/screens/bottom-tabs/WishlistScreen.tsx
+++ src/screens/bottom-tabs/WishlistScreen.tsx
@@ -1,30 +1,33 @@
 import { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Movie } from '../../types/Movies';
 import { getMovieList } from '../../utils/MovieService';
 import FastImage from 'react-native-fast-image';
+import { BottomTabsProp, RootStackScreenProp } from '../../navigation/type';
 
-const WishlistScreen = () => {
+const WishlistScreen = ({navigation} :BottomTabsProp<'Wishlist'>) => {
   const [movies, setMovies] = useState<Movie[]>([]);
   useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
     const fetchWatchList = async () => {
       const path = `account/22105497/watchlist/movies`;
       const data = await getMovieList(path);
 
-      setMovies(data);
-    };
-    fetchWatchList();
+      setMovies(data);}
+      fetchWatchList();
+    })
+     return unsubscribe;
   }, []);
 
   const renderMovieItem = ({ item }: { item: Movie }) => {
     return (
-      <View style={styles.img}>
+      <TouchableOpacity style={styles.img} onPress={() => navigation.navigate('Details', {movie_id:item.id})}>
         <FastImage
           source={{ uri: `https://image.tmdb.org/t/p/w200${item.poster_path}` }}
           style={{ width: '100%', height: '100%' }}
           resizeMode="contain"
         />
-      </View>
+      </TouchableOpacity>
     );
   };
 
@@ -36,6 +39,7 @@ const WishlistScreen = () => {
         keyExtractor={item => item.id.toString()}
         numColumns={4}
         contentContainerStyle={{ gap: 6 }}
+
       />
     </View>
   );
